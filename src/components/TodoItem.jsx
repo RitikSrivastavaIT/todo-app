a0556c25300efbd20b761cc65daa60277df45626
Ritik Srivastava <153807650+RitikSrivastavaIT@users.noreply.github.com>
2024-11-23 18:06:22 +0530

    Update TodoItem.jsx

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,14 +1,15 @@
 import React, { useContext } from "react";
 import { MdDelete } from "react-icons/md";
 import { TodoItemsContext } from "../store/todo-items-store";
+import styles from "./TodoItem.module.css";
 
 function TodoItem({ todoName, todoDate }) {
   const { deleteItem } = useContext(TodoItemsContext);
   return (
-    <div className="container">
+    <>
       <div className="row kg-row">
-        <div className="col-6">{todoName}</div>
-        <div className="col-4">{todoDate}</div>
+        <div className={`col-6 ${styles.itemContainer}`}>{todoName}</div>
+        <div className={`col-4 ${styles.itemContainer}`}>{todoDate}</div>
         <div className="col-2">
           <button
             type="button"
@@ -19,7 +20,7 @@ function TodoItem({ todoName, todoDate }) {
           </button>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
