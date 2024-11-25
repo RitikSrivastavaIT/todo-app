5e57ae8e07cfc3690c005eb36877a02a2dbb9982
Ritik Srivastava <ritiksrivastava.09.com>
2024-11-25 18:52:47 +0530

    Added useReducer functionality in the project.

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,19 @@
 import AddTodo from "./components/AddTodo";
 import AppName from "./components/AppName";
 import TodoItems from "./components/TodoItems";
-import { useState } from "react";
 import WelcomeMessage from "./components/WelcomeMessage";
-import { TodoItemsContext } from "./store/todo-items-store";
+import TodoItemsContextProvider from "./store/todo-items-store";
 
 function App() {
-  // const initialTodoItems = [
-  //   {
-  //     name: "Buy Milk",
-  //     date: "21/09/2024",
-  //   },
-  //   {
-  //     name: "Do Coding",
-  //     date: "24/09/2024",
-  //   },
-  //   {
-  //     name: "Book Reading",
-  //     date: "25/09/2024",
-  //   },
-  // ];
-  const [todoItems, setTodoItems] = useState([]);
-
-  const addNewItem = (itemName, itemDueDate) => {
-    setTodoItems((currValue) => [
-      ...currValue,
-      { name: itemName, date: itemDueDate },
-    ]);
-  };
-  const deleteItem = (todoItemName) => {
-    const newTodoItems = todoItems.filter((item) => item.name != todoItemName);
-    setTodoItems(newTodoItems);
-  };
-
   return (
-    <TodoItemsContext.Provider
-      value={{
-        todoItems,
-        addNewItem,
-        deleteItem,
-      }}
-    >
+    <TodoItemsContextProvider>
       <center className="todo-container">
         <AppName />
         <AddTodo />
         <WelcomeMessage />
         <TodoItems />
       </center>
-    </TodoItemsContext.Provider>
+    </TodoItemsContextProvider>
   );
 }
 
